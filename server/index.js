b9b67c0ad714f10487549cdfe26ec6820eb3e64a
cmccr <cmccrory548@gmail.com>
2022-04-06 13:54:30 -0600

    adjusted the h1 in css

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const app = express()
 
 app.use(express.static('public'))
 
+app.use(express.static('files'))
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
