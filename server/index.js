9279a4465fdd6e60f164854ab4cabe538161f2de
cmccr <cmccrory548@gmail.com>
2022-04-06 13:18:41 -0600

    adjusted HTML

--- server/index.js
+++ server/index.js
@@ -8,3 +8,7 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
+
+const PORT = process.env.PORT || 4005
+
+app.listen(PORT, () => { console.log(`listening on ${PORT}`)})
